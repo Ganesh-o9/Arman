abdc6cc8661bb3155f11a08c70443c0d819ad1cf
Ganesh-o9 <125977896+Ganesh-o9@users.noreply.github.com>
2025-03-06 13:22:23 +0530

    Update main3.py

--- main3.py
+++ main3.py
@@ -21,12 +21,20 @@ import os
 import re
 
 import requests
+
+botStartTime = time.time()
+batch = []
 bot = Client(
-    "CW",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
-)
+    "bot",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token)
+#bot = Client(
+#    "CW",
+#    bot_token=os.environ.get("BOT_TOKEN"),
+#    api_id=int(os.environ.get("API_ID")),
+#    api_hash=os.environ.get("API_HASH")
+#)
 
 logger = logging.getLogger()
 # thumb = os.environ.get("THUMB")
